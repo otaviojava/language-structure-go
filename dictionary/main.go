442269c524215e9ead61b7e3b2a5e0d13435c0c6
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-10 07:09:03 +0100

    Updates main go package

--- dictionary/main.go
+++ dictionary/main.go
@@ -1,1 +1,13 @@
-package dictionary
+package main
+
+import (
+	"fmt"
+	"language-structure/dictionary/java"
+)
+
+func main()  {
+	javaLanguage := java.NewRules()
+	fmt.Println(javaLanguage.Extensions)
+	fmt.Println(javaLanguage.Rules)
+	fmt.Println("Running code")
+}
