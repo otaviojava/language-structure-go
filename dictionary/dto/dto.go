e5d3559f5f36a91d31ba743fe0427063b88e15c3
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-10 10:59:23 +0100

    creates data transfer object file

--- dictionary/dto/dto.go
+++ dictionary/dto/dto.go
@@ -1,14 +1,48 @@
 package dto
 
-import "language-structure/dictionary/engine"
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"language-structure/dictionary/engine"
+)
 
 type Language struct {
 	Name        string
 	Extensions  []string
+	Rules []Rule
+}
+
+type Rule struct {
+	TYPE string
+	Metadata Metadata
 	Expressions []string
+}
+
+type Metadata struct {
+	Id string
+	Name string
+	Description string
+	Severity string
+	Confidence string
 }
 
 func (language Language) ToRuleManager() *engine.RuleManager {
 	rules := []engine.Rule{}
 	return engine.NewRuleManager(language.Name, rules, language.Extensions)
+}
+
+func ReadConf(filename string) (*Language, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Language{}
+	err = yaml.Unmarshal(buf, c)
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", filename, err)
+	}
+
+	return c, nil
 }
