e6f0cc7bc919a678a6df50508e3c9eaf9f1b1224
Otavio Santana <otaviopolianasantana@gmail.com>
2021-08-11 11:28:04 +0100

    creates data

--- dictionary/data/data.go
+++ dictionary/data/data.go
@@ -3,18 +3,23 @@ package data
 import "language-structure/dictionary/engine"
 
 type Languages struct {
-	data map[string]*engine.RuleManager
+	data map[string]engine.RuleManager
 }
 
-func (data Languages) Contains(key string) bool {
+func Create() Languages {
+	l := Languages{make(map[string]engine.RuleManager)}
+	return l
+}
+
+func (data *Languages) Contains(key string) bool {
 	_, ok := data.data[key]
 	return ok
 }
 
-func (data Languages) Size() int  {
+func (data *Languages) Size() int {
 	return len(data.data)
 }
 
-func (data Languages) Add(ruleManager *engine.RuleManager)  {
-	data.data[ruleManager.Name] = ruleManager
+func (data *Languages) Add(ruleManager *engine.RuleManager) {
+	data.data[ruleManager.Name] = *ruleManager
 }
